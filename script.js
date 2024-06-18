7899e64b8e5ac0dede362b08eabad769c4bef736
Sebastian <sebastianlakla@gmail.com>
2024-06-18 21:14:31 +0200

    Add event at query radio buttons

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 'use strict';
+const queryTypes = document.querySelectorAll('label[data-query-label]')
+queryTypes.forEach(el => {
+    el.addEventListener('click', (e) =>{
+        e.target.classList.toggle('query-radio-activated')
+        e.target.classList.toggle('general-enquiry-label-active')
+    })
+})
