475da9599f9773c4971b789121c3124d0527ffb6
Sebastian <sebastianlakla@gmail.com>
2024-06-22 11:02:36 +0200

    Add checked to false value to remove dot in radio

--- script.js
+++ script.js
@@ -4,5 +4,6 @@ queryTypes.forEach(el => {
     el.addEventListener('click', (e) =>{
         e.target.classList.toggle('query-radio-activated')
         e.target.classList.toggle('general-enquiry-label-active')
+        e.target.checked = false;
     })
 })
