9b843492e0a8ce2d4be191c995aec7969aa69768
Sebastian <sebastianlakla@gmail.com>
2024-06-24 20:35:05 +0200

    Change names of classes for name, last name, email input. Used it for script as variables

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 'use strict';
-const regexFnameLname = [A-Za-z];
-const regexEmail = /\S+@\S+\.\S+/;
+const modal = document.querySelector('.succes-modal')
+const nameInput = document.querySelector('.form__fname-input')
+const lastNameInput = document.querySelector('.form__lname-input')
+const emailInput = document.querySelector('.form__email-input')
 const queryTypes = document.querySelectorAll('label[data-query-label]')
+const textArea = document.querySelector('.form__text-area')
+const checkbox = document.querySelector('.clause-section__checkbox')
+const submitCTA = document.querySelector('.form__submit-button')
+const regexFnameLname = /[A-Za-z]/;
+const regexEmail = /\S+@\S+\.\S+/;
 queryTypes.forEach(el => {
     el.addEventListener('click', (e) =>{
         e.target.classList.toggle('query-radio-activated')
