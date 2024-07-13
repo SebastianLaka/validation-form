ea8ae5ce53a2a9fce974b482474f919d17624d7f
Sebastian <sebastianlakla@gmail.com>
2024-07-13 15:13:01 +0200

    Refacotred and fixed again bug with modal. There was problem when i didn't click in one of query then it showed

--- script.js
+++ script.js
@@ -15,57 +15,49 @@ const queryError = document.querySelector(".form__query-error");
 const textareaError = document.querySelector(".form__error-textarea");
 const checkboxError = document.querySelector(".form__checkbox-error");
 const regexEmail = /\S+@\S+\.\S+/;
+//function with global scope to valid form
 const testFieldName = (input) => {
   return input.value.length >= 3;
 };
-const addError = (error, input, succesModal) => {
+const addError = (error, input) => {
   error.classList.add("show-error");
   input.classList.add("error-border");
-  succesModal.style.transform = "translate(-50%, -150px)";
 };
-const removeError = (error, input, succesModal) => {
+const removeError = (error, input) => {
   error.classList.remove("show-error");
   input.classList.remove("error-border");
-  succesModal.style.transform = "translate(-50%, 50px)";
 };
+const validateField = (input, error, validFunc) => {
+  if (validFunc(input)) {
+    removeError(error, input);
+    return true;
+  } else {
+    addError(error, input);
+    return false;
+  }
+};
+//main validator at CTA button
 submitCTA.addEventListener("click", (e) => {
   e.preventDefault();
-  checkbox.addEventListener("click", (e) => {
-    if (checkbox.checked) {
-      e.target.classList.add("checkbox-activated");
-    } else {
-      e.target.classList.remove("checkbox-activated");
-    }
-  });
-  const validateFields = (input, error, validationFunc, succesModal) => {
-    if (validationFunc(input)) {
-      removeError(error, input, succesModal);
-    } else {
-      addError(error, input, succesModal);
-    }
-  };
-  validateFields(firstNameInput, nameError, testFieldName, modal);
-  validateFields(lastNameInput, surnameError, testFieldName, modal);
-  validateFields(emailInput, emailError, (input) => regexEmail.test(input.value), modal);
-
-  const validQuery = () => {
-    if (!queryGeneral.checked && !querySupport.checked) {
-      queryError.classList.add("show-error");
-      modal.style.transform = "translate(-50%, 50px)";
-    } else {
-      queryError.classList.remove("show-error");
-      modal.style.transform = "translate(-50%, -150px)";
-    }
-  };
-  validQuery();
-  validateFields(textArea, textareaError, (input) => input.value.length > 0, modal);
-  const validCheckbox = () => {
-    if (checkbox.checked) {
-      checkboxError.classList.remove("show-error");
-    } else {
-      checkboxError.classList.add("show-error");
-      modal.style.transform = "translate(-50%, -150px)";
-    }
-  };
-  validCheckbox();
+  const firstNameValid = validateField(firstNameInput, nameError, testFieldName);
+  const lastNameValid = validateField(lastNameInput, surnameError, testFieldName);
+  const emailValid = validateField(emailInput, emailError, (input) => regexEmail.test(input.value));
+  const textAreaValid = validateField(textArea, textareaError, (input) => input.value.length > 0);
+  const queryValid = queryGeneral.checked || querySupport.checked;
+  if (!queryValid) {
+    queryError.classList.add("show-error");
+  } else {
+    queryError.classList.remove("show-error");
+  }
+  const checkboxValid = checkbox.checked;
+  if (!checkboxValid) {
+    checkboxError.classList.add("show-error");
+  } else {
+    checkboxError.classList.remove("show-error");
+  }
+  if (firstNameValid && lastNameValid && emailValid && textAreaValid && queryValid && checkboxValid) {
+    modal.style.transform = "translate(-50%, 50px)";
+  } else {
+    modal.style.transform = "translate(-50%, -150px)";
+  }
 });
